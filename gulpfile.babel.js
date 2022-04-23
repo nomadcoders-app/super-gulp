d8db1773420931b322d5303d98df70349e2a4bd6
jinyongp <dev.jinyongp@gmail.com>
2022-04-24 02:11:14 +0900

    ✨ Image Optimization 작성

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,11 +1,14 @@
 import gulp from 'gulp';
-import gpug from 'gulp-pug';
+import gPug from 'gulp-pug';
 import webserver from 'gulp-webserver';
+import gImage from 'gulp-image';
 import del from 'del';
 
 const clean = () => del('dist');
 
-const pug = () => gulp.src('src/*.pug').pipe(gpug()).pipe(gulp.dest('dist'));
+const pug = () => gulp.src('src/*.pug').pipe(gPug()).pipe(gulp.dest('dist'));
+
+const img = () => gulp.src('src/img/*').pipe(gImage()).pipe(gulp.dest('dist/img'));
 
 const ws = () => gulp.src('dist').pipe(webserver({ livereload: true }));
 
@@ -13,6 +16,6 @@ const watch = () => gulp.watch('src/**/*.pug', pug);
 
 export const dev = gulp.series(
   gulp.parallel(clean),
-  gulp.parallel(pug),
+  gulp.parallel(pug, img),
   gulp.parallel(ws, watch),
 );
