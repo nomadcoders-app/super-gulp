e110d6f9af6a5f443d2b3268330db46471baad1e
jinyongp <dev.jinyongp@gmail.com>
2022-04-24 01:50:34 +0900

    ✨ Web Server and Watching Files 작성

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,13 +1,18 @@
 import gulp from 'gulp';
 import gpug from 'gulp-pug';
+import webserver from 'gulp-webserver';
 import del from 'del';
 
 const clean = () => del('dist');
 
 const pug = () => gulp.src('src/*.pug').pipe(gpug()).pipe(gulp.dest('dist'));
 
-const prepare = gulp.series(clean);
+const ws = () => gulp.src('dist').pipe(webserver({ livereload: true }));
 
-const compile = gulp.series(pug);
+const watch = () => gulp.watch('src/**/*.pug', pug);
 
-export const dev = gulp.series(prepare, compile);
+export const dev = gulp.series(
+  gulp.parallel(clean),
+  gulp.parallel(pug),
+  gulp.parallel(ws, watch),
+);
