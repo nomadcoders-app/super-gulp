1f9fdaf30ddbd7e12115fd84afb9d0b5114dcd9f
jinyongp <dev.jinyongp@gmail.com>
2022-04-24 01:15:16 +0900

    ✨ Pug Compilation 작성

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,1 +1,13 @@
 import gulp from 'gulp';
+import gpug from 'gulp-pug';
+import del from 'del';
+
+const clean = () => del('dist');
+
+const pug = () => gulp.src('src/*.pug').pipe(gpug()).pipe(gulp.dest('dist'));
+
+const prepare = gulp.series(clean);
+
+const compile = gulp.series(pug);
+
+export const dev = gulp.series(prepare, compile);
