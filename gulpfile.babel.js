a05d82b9626add369cb3402989c26d82cb2088ce
jinyongp <dev.jinyongp@gmail.com>
2022-04-24 12:38:55 +0900

    ✨ SCSS Compilation 작성

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,21 +1,38 @@
 import gulp from 'gulp';
-import gPug from 'gulp-pug';
+import pug from 'gulp-pug';
 import webserver from 'gulp-webserver';
-import gImage from 'gulp-image';
 import del from 'del';
+import image from 'gulp-image';
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import autoprefixer from 'gulp-autoprefixer';
+import csso from 'gulp-csso';
+
+const sass = gulpSass(dartSass);
 
 const clean = () => del('dist');
 
-const pug = () => gulp.src('src/*.pug').pipe(gPug()).pipe(gulp.dest('dist'));
+const html = () => gulp.src('src/index.pug').pipe(pug()).pipe(gulp.dest('dist'));
 
-const img = () => gulp.src('src/img/*').pipe(gImage()).pipe(gulp.dest('dist/img'));
+const img = () => gulp.src('src/img/*').pipe(image()).pipe(gulp.dest('dist/img'));
+
+const css = () =>
+  gulp
+    .src('src/scss/style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(csso())
+    .pipe(gulp.dest('dist/css'));
 
 const ws = () => gulp.src('dist').pipe(webserver({ livereload: true }));
 
-const watch = () => gulp.watch('src/**/*.pug', pug);
+const watch = () => {
+  gulp.watch('src/**/*.pug', html);
+  gulp.watch('src/**/*.scss', css);
+};
 
 export const dev = gulp.series(
   gulp.parallel(clean),
-  gulp.parallel(pug, img),
+  gulp.parallel(html, css, img),
   gulp.parallel(ws, watch),
 );
