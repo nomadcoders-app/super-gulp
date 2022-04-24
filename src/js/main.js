954506b58f68679c31eaa6683c37688711123602
jinyongp <dev.jinyongp@gmail.com>
2022-04-24 15:38:24 +0900

    ✨ JS 코드 업데이트
    - 커스텀 커서 추가
    - 커서 움직임에 따른 그림자 효과 적용

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,38 @@
-import { random } from './util';
+import { getCenterPosition, getCursorPosition, getTransformStyle } from './util';
 
-const randOne = random(999);
-const randTwo = random(999);
+window.addEventListener('DOMContentLoaded', () => {
+  document.body.style.cursor = 'none';
 
-console.log(`Lucky Numbers: ${randOne} ${randTwo}`);
+  const logo = document.querySelector('#main-logo');
+
+  const cursor = document.querySelector('.cursor');
+  cursor.style.visibility = 'hidden';
+  const cursorSize = 32;
+
+  let [centerX, centerY] = getCenterPosition(logo);
+
+  window.addEventListener('resize', () => {
+    [centerX, centerY] = getCenterPosition(logo);
+  });
+
+  document.body.addEventListener('pointermove', (event) => {
+    cursor.style.visibility = 'visible';
+    const [x, y] = getCursorPosition(event);
+    const dX = (centerX - x) / 30;
+    const dY = (centerY - y) / 30;
+    const hyp = Math.hypot(centerX - x, centerY - y);
+    const rad = Math.atan2(centerY - y, centerX - x);
+    const deg = rad * (180 / Math.PI) + 225;
+    logo.style.filter = `drop-shadow(${dX}px ${dY}px 2px rgba(0 0 0 / 30%))`;
+    cursor.style.transform = getTransformStyle(
+      x - cursorSize / 2,
+      y - cursorSize / 2,
+      deg,
+      0.5 + hyp / 500,
+    );
+  });
+
+  document.body.addEventListener('pointerleave', () => {
+    cursor.style.visibility = 'hidden';
+  });
+});
--- src/js/util.js
+++ src/js/util.js
@@ -1,1 +1,9 @@
-export const random = (max) => Math.round(Math.random() * max);
+export const getCursorPosition = (event) => [event.clientX, event.clientY];
+
+export const getCenterPosition = (element) => [
+  element.offsetLeft + element.clientWidth / 2,
+  element.offsetTop + element.clientHeight / 2,
+];
+
+export const getTransformStyle = (x, y, d, s) =>
+  `translate(${x || 0}px, ${y || 0}px) rotate(${d || 0}deg) scale(${s || 1})`;
