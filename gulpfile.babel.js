60151c3fab51ab42d3313d7ba58668ef1f64125a
jinyongp <dev.jinyongp@gmail.com>
2022-04-24 15:37:07 +0900

    ✨ Babel Support 작성

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,6 +7,8 @@ import dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 import autoprefixer from 'gulp-autoprefixer';
 import csso from 'gulp-csso';
+import browserify from 'gulp-bro';
+import babelify from 'babelify';
 
 const sass = gulpSass(dartSass);
 
@@ -24,15 +26,30 @@ const css = () =>
     .pipe(csso())
     .pipe(gulp.dest('dist/css'));
 
+const js = () =>
+  gulp
+    .src('src/js/main.js')
+    .pipe(
+      browserify({
+        debug: process.env.NODE_ENV === 'development',
+        transform: [
+          babelify.configure({ presets: ['@babel/preset-env'] }),
+          ['uglifyify', { global: true }],
+        ],
+      }),
+    )
+    .pipe(gulp.dest('dist/js'));
+
 const ws = () => gulp.src('dist').pipe(webserver({ livereload: true }));
 
 const watch = () => {
   gulp.watch('src/**/*.pug', html);
   gulp.watch('src/**/*.scss', css);
+  gulp.watch('src/**/*.js', js);
 };
 
 export const dev = gulp.series(
   gulp.parallel(clean),
-  gulp.parallel(html, css, img),
+  gulp.parallel(html, css, js, img),
   gulp.parallel(ws, watch),
 );
